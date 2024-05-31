c4ec59afcc9498d71cd53e9abf9a0bffb03fe4f8
Beverleyxv <nyeletibev@gmail.com>
2024-05-31 17:22:39 +0200

    Updated weather details

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let city = document.querySelector("#city");
+  let date = new Date(response.data.time * 1000);
+  let time = document.querySelector("#time");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind-speed");
 
   city.innerHTML = response.data.city;
+  time.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
+  description.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  wind.innerHTML = `${response.data.wind.speed}km/h`;
+  console.log(response.data);
 }
 function searchCity(city) {
   let apiKey = "a3ba8cdc7c701foca4e9dta8280cb609";
@@ -16,6 +26,26 @@ function submitSearch(event) {
   let searchInput = document.querySelector("#search-input");
 
   searchCity(searchInput.value);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", submitSearch);
