abd58617d8f71462f1f93dedcdd3831e46a9375d
Beverleyxv <nyeletibev@gmail.com>
2024-06-02 12:18:11 +0200

    Introduced weather forecast

--- script.js
+++ script.js
@@ -16,18 +16,10 @@ function refreshWeather(response) {
   description.innerHTML = response.data.condition.description;
   humidity.innerHTML = `${response.data.temperature.humidity}%`;
   wind.innerHTML = `${response.data.wind.speed}km/h`;
-}
-function searchCity(city) {
-  let apiKey = "a3ba8cdc7c701foca4e9dta8280cb609";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  axios.get(apiUrl).then(refreshWeather);
-}
-function submitSearch(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
 
-  searchCity(searchInput.value);
+  getForecast(response.data.city);
 }
+
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
@@ -48,6 +40,59 @@ function formatDate(date) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+
+function searchCity(city) {
+  let apiKey = "a3ba8cdc7c701foca4e9dta8280cb609";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function submitSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+
+  searchCity(searchInput.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+function getForecast(city) {
+  let apiKey = "a3ba8cdc7c701foca4e9dta8280cb609";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml += 
+
+        ` <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>${Math.round(day.temperature.maximum)}º</strong>
+        <div class="weather-forecast-temperature">
+          <strong>${Math.round(day.temperature.minimum)}º</strong>
+        </div>
+      </div>
+    </div>
+    </div>`;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", submitSearch);
 
